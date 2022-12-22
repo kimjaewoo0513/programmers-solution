a9307b81f14ee982307dfa995b8d007bb05536f3
kimjaewoo0513 <howchamsin@gmail.com>
2022-12-22 16:58:24 +0900

    3진법 뒤집기

--- src/level1/Q034.java
+++ src/level1/Q034.java
@@ -1,16 +1,18 @@
-package level1;
+// package level1;
 
-public class Q034 {
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        System.out.println(s.solution(125));
-    }
-}
+// public class Q034 {
+//     public static void main(String[] args) {
+//         Solution s = new Solution();
+//         System.out.println(s.solution(125));
+//     }
+// }
 
-//3진법 뒤집기
-class Solution {
-    public int solution(int n) {
-        int answer = 0;
-        return answer;
-    }
-}
+// //3진법 뒤집기
+// class Solution {
+//     public int solution(int n) {
+//         int answer = 0;
+//         StringBuffer sb = new StringBuffer(Integer.toString(n, 3));
+//         String reverse = sb.reverse().toString();
+//         return answer = Integer.parseInt(reverse,3);
+//     }
+// }
