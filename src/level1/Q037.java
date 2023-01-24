31c6676104fd7000fc295807ad0c0ed6444530c8
kimjaewoo0513 <howchamsin@gmail.com>
2023-01-24 19:19:45 +0900

    커밋

--- src/level1/Q037.java
+++ src/level1/Q037.java
@@ -15,44 +15,32 @@ public class Q037 {
                             ,{14, 7}
                             ,{5, 15} };
 
-        System.out.println( s.solution(sizes) );
+        System.out.println("답 = " + s.solution(sizes) );
     }
 }
 
-
-/*
-    최소 직사각형
-    1. 모든 배열의 0번째 요소끼리 비교 -> 가장 큰 값, 가장 작은 값 저장
-    2. 모든 배열의 1번째 요소끼리 비교 -> 가장 큰 값, 가장 작은 값 저장
-    3. 가장 큰 세로 길이가 가장 큰 가로 길이보다 작거나 같고, 그 배열의 가로 길이가 가장 큰 세로 길이 보다 작거나 같을 때
-       순서를 바꾸고 계산 시작.
- */
+// tip. 모든 명함들의 긴 모서리를 가로로 돌려서 겹쳐봅시다.
+//      그 상태에서 지갑에 한번에 넣을 수 있으려면 세로길이는 어떻게 해야할지 상상해봅시다!
 class Solution {
     public int solution(int[][] sizes) {
-        int answer = 0;
-
-        int maxW = sizes[0][0];
-        int minW = sizes[0][0];
-
-        int maxH = sizes[0][1];
-        int minH = sizes[0][1];
+        int width = 0;
+        int height = 0;
 
         for (int i = 0; i < sizes.length; i++) {
-            if(maxW < sizes[i][0]) maxW = sizes[i][0];
-            if(minW > sizes[i][0]) minW = sizes[i][0];
-
-
-            // 예제 2번에서 순서 바귀지 않는 현상 있음
-            if(maxH <= maxW && sizes[i][0] <= maxH){
-                int temp = sizes[i][0];
+            if(sizes[i][0] < sizes[i][1]){
+                int temp;
+                temp = sizes[i][0];
                 sizes[i][0] = sizes[i][1];
                 sizes[i][1] = temp;
             }
 
-            if(maxH < sizes[i][1]) maxH = sizes[i][1];
-            if(minH > sizes[i][1]) minH = sizes[i][1];
+            if(width < sizes[i][0]) 
+                width = sizes[i][0];
+            if(height < sizes[i][1]) 
+                height = sizes[i][1];
         }
-
-        return answer = maxH * maxW;
+        System.out.println("최대 가로길이  = " + width);
+        System.out.println("최대 세로길이  = " + height);
+        return width * height;
     }
 }
---
+++ src/level1/Q038.java
@@ -0,0 +1,15 @@
+package level1;
+
+public class Q038 {
+    public static void main(String[] args) {
+        
+    }
+}
+
+//비밀지도
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = {};
+        return answer;
+    }
+}
