25bb531f6dc5c66d69da9e1c5ce824c92b9ff6dc
kimjaewoo0513 <howchamsin@gmail.com>
2023-01-26 20:06:45 +0900

    비밀지도 진행중

--- src/level1/Q037.java
+++ src/level1/Q037.java
@@ -1,46 +1,46 @@
-package level1;
+// package level1;
 
-public class Q037 {
-    public static void main(String[] args) {
-        Solution s = new Solution();
+// public class Q037 {
+//     public static void main(String[] args) {
+//         Solution s = new Solution();
 
-        int[][] sizes1 = {  {60, 50}
-                           ,{30, 70}
-                           ,{60, 30}
-                           ,{80, 40} };
+//         int[][] sizes1 = {  {60, 50}
+//                            ,{30, 70}
+//                            ,{60, 30}
+//                            ,{80, 40} };
 
-        int[][] sizes = {    {10, 7}
-                            ,{12, 3}
-                            ,{8, 15}
-                            ,{14, 7}
-                            ,{5, 15} };
+//         int[][] sizes = {    {10, 7}
+//                             ,{12, 3}
+//                             ,{8, 15}
+//                             ,{14, 7}
+//                             ,{5, 15} };
 
-        System.out.println("답 = " + s.solution(sizes) );
-    }
-}
+//         System.out.println("답 = " + s.solution(sizes) );
+//     }
+// }
 
-// tip. 모든 명함들의 긴 모서리를 가로로 돌려서 겹쳐봅시다.
-//      그 상태에서 지갑에 한번에 넣을 수 있으려면 세로길이는 어떻게 해야할지 상상해봅시다!
-class Solution {
-    public int solution(int[][] sizes) {
-        int width = 0;
-        int height = 0;
+// // tip. 모든 명함들의 긴 모서리를 가로로 돌려서 겹쳐봅시다.
+// //      그 상태에서 지갑에 한번에 넣을 수 있으려면 세로길이는 어떻게 해야할지 상상해봅시다!
+// class Solution {
+//     public int solution(int[][] sizes) {
+//         int width = 0;
+//         int height = 0;
 
-        for (int i = 0; i < sizes.length; i++) {
-            if(sizes[i][0] < sizes[i][1]){
-                int temp;
-                temp = sizes[i][0];
-                sizes[i][0] = sizes[i][1];
-                sizes[i][1] = temp;
-            }
+//         for (int i = 0; i < sizes.length; i++) {
+//             if(sizes[i][0] < sizes[i][1]){
+//                 int temp;
+//                 temp = sizes[i][0];
+//                 sizes[i][0] = sizes[i][1];
+//                 sizes[i][1] = temp;
+//             }
 
-            if(width < sizes[i][0]) 
-                width = sizes[i][0];
-            if(height < sizes[i][1]) 
-                height = sizes[i][1];
-        }
-        System.out.println("최대 가로길이  = " + width);
-        System.out.println("최대 세로길이  = " + height);
-        return width * height;
-    }
-}
+//             if(width < sizes[i][0]) 
+//                 width = sizes[i][0];
+//             if(height < sizes[i][1]) 
+//                 height = sizes[i][1];
+//         }
+//         System.out.println("최대 가로길이  = " + width);
+//         System.out.println("최대 세로길이  = " + height);
+//         return width * height;
+//     }
+// }
--- src/level1/Q038.java
+++ src/level1/Q038.java
@@ -1,7 +1,15 @@
 package level1;
 
+import java.util.ArrayList;
+
 public class Q038 {
     public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int n = 5;
+        int arr1[] = {9, 20, 28, 18, 11};
+        int arr2[] = {30, 1, 21, 17, 28};
+        System.out.println(s.solution(n, arr1, arr2));
         
     }
 }
@@ -10,6 +18,25 @@ public class Q038 {
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = {};
+
+        ArrayList<Integer> map1 = new ArrayList<>();
+        ArrayList<Integer> map2 = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String binaryString1 = Integer.toBinaryString(arr1[i]);
+            String binaryString2 = Integer.toBinaryString(arr2[i]);
+
+            // map1(i)의 길이가 n보다 작을때 n만큼 앞에 0을 붙여주는 작업 필요
+            if(){
+
+            }
+
+            map1.add( Integer.parseInt(binaryString1) );
+            map2.add( Integer.parseInt(binaryString2) );
+        }
+
+        for (Integer i : map2) System.out.println(i);
+
         return answer;
     }
 }
