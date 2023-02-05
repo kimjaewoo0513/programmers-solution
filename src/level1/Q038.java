cda4279db3e0d2d9276d2ac55392f578c981bfd0
kimjaewoo0513 <howchamsin@gmail.com>
2023-02-05 20:53:59 +0900

    비밀지도 하는중

--- src/level1/Q038.java
+++ src/level1/Q038.java
@@ -19,24 +19,51 @@ class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = {};
 
-        ArrayList<Integer> map1 = new ArrayList<>();
-        ArrayList<Integer> map2 = new ArrayList<>();
+        ArrayList<String> map1 = new ArrayList<>();
+        ArrayList<String> map2 = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
-            String binaryString1 = Integer.toBinaryString(arr1[i]);
-            String binaryString2 = Integer.toBinaryString(arr2[i]);
-
-            // map1(i)의 길이가 n보다 작을때 n만큼 앞에 0을 붙여주는 작업 필요
-            if(){
-
-            }
-
-            map1.add( Integer.parseInt(binaryString1) );
-            map2.add( Integer.parseInt(binaryString2) );
+            map1.add( String.format("%05d", Integer.parseInt(Integer.toBinaryString(arr1[i]).toString())) );
+            map2.add( String.format("%05d", Integer.parseInt(Integer.toBinaryString(arr2[i]).toString())) );
         }
 
-        for (Integer i : map2) System.out.println(i);
+        // 두 배열을 2진수로 변환하여 list에 add 작업까지 완료
+
+        for (String i : map1) System.out.println(i);
+        for (String i : map2) System.out.println(i);
 
         return answer;
     }
 }
+
+/*
+
+        String binaryString1 = Integer.toString(arr1[i],2);
+        String binaryString2 = Integer.toString(arr2[i],2);
+            
+        if(binaryString1.length() < n){
+            for (int index = 0; index < n - binaryString1.length() ; index++) 
+                binaryString1 = "0" + binaryString1;
+            map1.add( binaryString1 );
+        }else{
+            map1.add( binaryString1 );
+        }
+
+        if(binaryString2.length() < n){
+            for (int index = 0; index < n - binaryString2.length() ; index++) 
+                binaryString2 = "0" + binaryString2;
+            map2.add( binaryString2 );
+        }else{
+            map2.add( binaryString2 );
+        }
+
+
+       이렇게 긴 식이 
+
+        ===> 
+
+        map1.add( String.format("%05d", Integer.parseInt(Integer.toBinaryString(arr1[i]).toString())) );
+
+        로 정리 됨 
+
+ */
