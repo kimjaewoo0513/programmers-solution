b01ff5d8a5635238aee522f216b3563bc24562ac
kimjaewoo0513 <howchamsin@gmail.com>
2023-02-08 15:09:22 +0900

    비밀지도 성공

--- src/level1/Q038.java
+++ src/level1/Q038.java
@@ -1,14 +1,12 @@
 package level1;
 
-import java.util.ArrayList;
-
 public class Q038 {
     public static void main(String[] args) {
         Solution s = new Solution();
 
-        int n = 5;
-        int arr1[] = {9, 20, 28, 18, 11};
-        int arr2[] = {30, 1, 21, 17, 28};
+        int n = 6;
+        int arr1[] = {46, 33, 33 ,22, 31, 50};
+        int arr2[] = {27 ,56, 19, 14, 14, 10};
         System.out.println(s.solution(n, arr1, arr2));
         
     }
@@ -17,21 +15,12 @@ public class Q038 {
 //비밀지도
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = {};
-
-        ArrayList<String> map1 = new ArrayList<>();
-        ArrayList<String> map2 = new ArrayList<>();
+        String[] answer = new String[n];
 
         for (int i = 0; i < n; i++) {
-            map1.add( String.format("%05d", Integer.parseInt(Integer.toBinaryString(arr1[i]).toString())) );
-            map2.add( String.format("%05d", Integer.parseInt(Integer.toBinaryString(arr2[i]).toString())) );
+            answer[i] = String.format("%0" + n + "d", Long.parseLong(Long.toBinaryString(arr1[i] | arr2[i]).toString())).replace("1", "#");
+            answer[i] = answer[i].replace("0", " ");
         }
-
-        // 두 배열을 2진수로 변환하여 list에 add 작업까지 완료
-
-        for (String i : map1) System.out.println(i);
-        for (String i : map2) System.out.println(i);
-
         return answer;
     }
 }
@@ -67,3 +56,19 @@ class Solution {
         로 정리 됨 
 
  */
+
+    /* 리스트 활용 버전
+        ArrayList<String> map1 = new ArrayList<>();
+        ArrayList<String> map2 = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            map1.add( String.format("%05d", Integer.parseInt(Integer.toBinaryString(arr1[i]).toString())) );
+            map2.add( String.format("%05d", Integer.parseInt(Integer.toBinaryString(arr2[i]).toString())) );
+        }
+         
+
+        // 두 배열을 2진수로 변환하여 list에 add 작업까지 완료
+
+        for (String i : map1) System.out.println(i);
+        for (String i : map2) System.out.println(i);
+        */
