ab98414c9a50ccbe3c5bbf4d7742efdb269a04da
kimjaewoo0513 <howchamsin@gmail.com>
2023-02-22 15:45:55 +0900

    두 개 뽑아서 더하기 풀이중

--- src/level1/Q041.java
+++ src/level1/Q041.java
@@ -1,28 +1,28 @@
-package level1;
+// package level1;
 
-public class Q041 {
+// public class Q041 {
 
-    public static void main(String[] args) {
-        Solution ss = new Solution();
-        String s = "2three45sixseven";
-        System.out.println(ss.solution(s));
-    }
-}
+//     public static void main(String[] args) {
+//         Solution ss = new Solution();
+//         String s = "2three45sixseven";
+//         System.out.println(ss.solution(s));
+//     }
+// }
 
-// 숫자 문자열과 영단어
-class Solution {
-    public int solution(String s) {
-        int answer = 0;
+// // 숫자 문자열과 영단어
+// class Solution {
+//     public int solution(String s) {
+//         int answer = 0;
 
-        String [] engNumber = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+//         String [] engNumber = {"zero","one","two","three","four","five","six","seven","eight","nine"};
 
-        for (int i = 0; i < engNumber.length; i++) {
-            String replaceNumber = Integer.toString(i);
-            if(s.contains(engNumber[i]))
-                s = s.replaceAll(engNumber[i], replaceNumber);
-            else
-                continue;
-        }
-        return answer = Integer.parseInt(s);
-    }
-}
+//         for (int i = 0; i < engNumber.length; i++) {
+//             String replaceNumber = Integer.toString(i);
+//             if(s.contains(engNumber[i]))
+//                 s = s.replaceAll(engNumber[i], replaceNumber);
+//             else
+//                 continue;
+//         }
+//         return answer = Integer.parseInt(s);
+//     }
+// }
---
+++ src/level1/Q042.java
@@ -0,0 +1,17 @@
+package level1;
+
+public class Q042 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int [] numbers = {2,1,3,4,1};
+        System.out.println(s.solution(numbers));
+    }
+}
+//두 개 뽑아서 더하기
+class Solution {
+    public int[] solution(int[] numbers) {
+        int[] answer = {};
+        return answer;
+    }
+}
