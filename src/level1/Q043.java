ff0444d90fa5d1fbcbf9cdd493679dd9e1d2a0ee
kimjaewoo0513 <howchamsin@gmail.com>
2023-02-22 17:30:04 +0900

    크기가 작은 부분문자열 시작

---
+++ src/level1/Q043.java
@@ -0,0 +1,19 @@
+package level1;
+
+public class Q043 {
+    public static void main(String[] args) {
+        Solution s = new  Solution();
+        String t = "3141592";
+        String p = "271";// {"500220839878"	"7"} , {"10203"	"15"}
+
+        System.out.println(s.solution(t, p));
+    }
+}
+
+//크기가 작은 부분문자열
+class Solution {
+    public int solution(String t, String p) {
+        int answer = 0;
+        return answer;
+    }
+}
