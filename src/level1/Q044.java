2da4c87b5fe6f101fba50d849421997bc0f39d47
kimjaewoo0513 <howchamsin@gmail.com>
2023-02-24 15:17:18 +0900

    [카카오 인턴] 키패드 누르기 시작 => 정답률 50% 이하 위주 풀이 시작

--- src/level1/Q044.java
+++ src/level1/Q044.java
@@ -2,6 +2,20 @@ package level1;
 
 public class Q044 {
     public static void main(String[] args) {
+        Solution s = new  Solution();
+        int [] numbers = {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
+        String hand = "right";
+        System.out.println(s.solution(numbers, hand));
+    }
+}
+
+//[카카오 인턴] 키패드 누르기
+class Solution {
+    public String solution(int[] numbers, String hand) {
+        String answer = "";
+
         
+
+        return answer;
     }
 }
